67504eb5bfdf1e90d9d4197e60fb66cebce1ae11
Chris Pachomski <cpachomski@gmail.com>
2019-01-04 02:32:13 -0500

    use localStorage + codeEvaluation hooks in Exercise container

--- src/pages/excercise/index.js
+++ src/pages/excercise/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import Editor from "containers/editor";
 import SideNav from "containers/side-nav";
+import useLocalStorage from "hooks/use-local-storage";
+import useCodeEvaluator from "hooks/use-code-evaluator";
 
 const Layout = styled.div`
   display: grid;
@@ -11,9 +14,28 @@ const Layout = styled.div`
   width: 100vw;
 `;
 
-export default () => (
-  <Layout>
-    <SideNav text="sup" />
-    <Editor />
-  </Layout>
-);
+function Exercise({ name, initialCode }) {
+  const codeKey = `hardjs-${name}`;
+  const [code, setCode] = useLocalStorage(codeKey, initialCode);
+  const [evaluation, evaluate] = useCodeEvaluator(code);
+
+  return (
+    <Layout>
+      <div>
+        <button onClick={() => evaluate(code)}>
+          {evaluation.computing ? "Running" : "Evaluate"}
+        </button>
+        {evaluation.answer && <span>Answer: {evaluation.answer}</span>}
+        <SideNav />
+      </div>
+      <Editor snippetName={name} code={code} setCode={setCode} />
+    </Layout>
+  );
+}
+
+Exercise.propTypes = {
+  name: PropTypes.string,
+  initialCode: PropTypes.string
+};
+
+export default Exercise;
