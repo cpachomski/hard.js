08756377484276cebf81a94d6eefc8ae1d1d2c39
Chris Pachomski <cpachomski@gmail.com>
2019-01-04 02:31:10 -0500

    pass down first test exercise to Exercise component from App top level

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "components/header";
 import Excercise from "pages/excercise";
 
+import exercises from "exercises";
+
 class App extends React.Component {
   render() {
+    const { name, code } = exercises[0];
+
     return (
       <>
         <Header appName={"hard.js"} />
-        <Excercise />
+        <Excercise name={name} initialCode={code} />
       </>
     );
   }
