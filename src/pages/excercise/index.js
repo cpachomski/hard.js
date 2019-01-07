af3c7227c5cdd28ea968463a695893ac853557ba
Chris Pachomski <cpachomski@gmail.com>
2019-01-07 01:12:18 -0500

    update layout to put result box beneath code

--- src/pages/excercise/index.js
+++ src/pages/excercise/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Editor from "containers/editor";
-import SideNav from "containers/side-nav";
+import ResultBox from "containers/result-box";
 import useLocalStorage from "hooks/use-local-storage";
-import useCodeEvaluator from "hooks/use-code-evaluator";
 
 const Layout = styled.div`
   display: grid;
@@ -12,23 +11,31 @@ const Layout = styled.div`
   margin-top: 50px;
   height: calc(100vh - 50px);
   width: 100vw;
+
+  > *:first-child {
+    grid-column: 1;
+  }
+
+  > *: nth-child(2) {
+    grid-column: 2;
+    grid-row: 1/3;
+  }
+
+  >*: nth-child(3) {
+    grid-column: 2;
+    grid-row: 3;
+  }
 `;
 
 function Exercise({ name, initialCode }) {
   const codeKey = `hardjs-${name}`;
   const [code, setCode] = useLocalStorage(codeKey, initialCode);
-  const [evaluation, evaluate] = useCodeEvaluator(code);
 
   return (
     <Layout>
-      <div>
-        <button onClick={() => evaluate(code)}>
-          {evaluation.computing ? "Running" : "Evaluate"}
-        </button>
-        {evaluation.answer && <span>Answer: {evaluation.answer}</span>}
-        <SideNav />
-      </div>
+      <div />
       <Editor snippetName={name} code={code} setCode={setCode} />
+      <ResultBox submission={code} />
     </Layout>
   );
 }
